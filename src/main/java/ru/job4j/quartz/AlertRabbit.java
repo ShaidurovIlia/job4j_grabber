53f2460631c361a269cb6322f649f8f25389b683
Admin <shaidurov.ilha@yandex.ru>
2022-08-16 16:42:36 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,6 +4,10 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -14,27 +18,59 @@ public class AlertRabbit {
     public static void main(String[] args) {
         try {
             Properties properties = readProperties();
+            List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail jod = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(properties
-                            .getProperty("rabbit.interval")))
-                    .repeatForever();
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(jod, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+            JobDataMap data = new JobDataMap();
+            data.put("store", store);
+            try (Connection connection = createConnection(properties)) {
+                data.put("connection", connection);
+                JobDetail jod = newJob(Rabbit.class)
+                        .usingJobData(data)
+                        .build();
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(Integer.parseInt(properties
+                                .getProperty("rabbit.interval")))
+                        .repeatForever();
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                scheduler.scheduleJob(jod, trigger);
+                Thread.sleep(10000);
+                scheduler.shutdown();
+            }
+            System.out.println(store);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
+            List<Long> store = (List<Long>) context
+                    .getJobDetail()
+                    .getJobDataMap()
+                    .get("store");
+            store.add(System.currentTimeMillis());
+            Connection cn = (Connection) context
+                    .getJobDetail()
+                    .getJobDataMap()
+                    .get("connection");
+            try (PreparedStatement ps = cn.prepareStatement(
+                    "insert into rabbit (created_date) values(?)")) {
+                ps.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                ps.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -43,11 +79,21 @@ public class AlertRabbit {
         try (InputStream in = AlertRabbit.class
                 .getClassLoader()
                 .getResourceAsStream(
-                "rabbit.properties")) {
+                        "rabbit.properties")) {
             properties.load(in);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return properties;
+    }
+
+    private static Connection createConnection(Properties properties)
+            throws ClassNotFoundException, SQLException {
+        Class.forName(properties.getProperty("jdbc.connection.driver-class-name"));
+        return DriverManager.getConnection(
+                properties.getProperty("jdbc.connection.url"),
+                properties.getProperty("jdbc.connection.username"),
+                properties.getProperty("jdbc.connection.password")
+        );
     }
 }
